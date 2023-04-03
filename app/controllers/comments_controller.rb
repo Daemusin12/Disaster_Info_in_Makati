99ae1a1071b5c267654870f1279941d2ac5e7eb8
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-03 14:59:32 +0800

    remove comment show routes, added edit and update method in commentscontroller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
 
   def index
     @comments = @post.comments
@@ -18,6 +19,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_comments_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_post
@@ -26,5 +37,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 end
