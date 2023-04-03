be8d05607f8894f369e92f747897943368ef1680
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-03 14:34:08 +0800

    create new and create method in commentscontroller, added comments new.html, added comment simple validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,26 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
+  def new
+    @comment = @post.comments.build
+  end
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
