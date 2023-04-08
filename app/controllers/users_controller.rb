c1146c40e2826c9824f84da7b3a2b31bd44cf649
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-08 15:58:39 +0800

    added authenticate user in user controller, remove index link at post new.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @posts = Post.includes(:disasters, :user, :comments).where(user_id: current_user.id)
   end
